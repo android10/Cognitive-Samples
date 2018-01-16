6f1aef2f1e05bf23809f3c13a857ad6fdaaa4d99
Fernando Cejas <fcejas@gmail.com>
2018-01-16 13:57:00 +0100

    Initial commit for android chatbot project

---
+++ chatbot/android/app/src/main/java/com/fernando/cognitive/chatbot/ChatActivity.kt
@@ -0,0 +1,12 @@
+package com.fernando.cognitive.chatbot
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ChatActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+    }
+}
