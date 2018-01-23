602dc15599bbad2a91bb98b1fcb0eb1eef602602
Fernando Cejas <fcejas@gmail.com>
2018-01-23 21:49:11 +0100

    Add View extension method.

---
+++ chatbot/android/app/src/main/kotlin/com/fernandocejas/cognitive/chatbot/extension/Views.kt
@@ -0,0 +1,8 @@
+package com.fernandocejas.cognitive.chatbot.extension
+
+import android.support.annotation.LayoutRes
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+fun ViewGroup.inflate(@LayoutRes layoutRes: Int): View = LayoutInflater.from(context).inflate(layoutRes, this, false)
