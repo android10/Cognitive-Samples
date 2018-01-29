3d28ce42c9cdc1990658f2cf1e0cb977cf905041
Fernando Cejas <fcejas@gmail.com>
2018-01-29 16:56:36 +0100

    Retouch UI a little bit for message appeareance.

--- chatbot/android/app/src/main/kotlin/com/fernandocejas/cognitive/chatbot/chat/MessageViewHolder.kt
+++ chatbot/android/app/src/main/kotlin/com/fernandocejas/cognitive/chatbot/chat/MessageViewHolder.kt
@@ -2,8 +2,10 @@ package com.fernandocejas.cognitive.chatbot.chat
 
 import android.support.v7.widget.RecyclerView
 import android.view.View
-//import kotlinx.android.synthetic.main.item_message_received.view.text_message
-//import kotlinx.android.synthetic.main.item_message_sent.view.text_message
+import kotlinx.android.synthetic.main.item_message_received.view.date_time_received
+import kotlinx.android.synthetic.main.item_message_received.view.text_message_received
+import kotlinx.android.synthetic.main.item_message_sent.view.date_time_sent
+import kotlinx.android.synthetic.main.item_message_sent.view.text_message_sent
 
 sealed class MessageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
@@ -21,13 +23,15 @@ sealed class MessageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemVie
 
     internal class MessageSentViewHolder(itemView: View) : MessageViewHolder(itemView) {
         override fun bind(messageViewModel: MessageViewModel) {
-//            itemView.text_message.text = messageViewModel.message
+            itemView.text_message_sent.text = messageViewModel.message
+            itemView.date_time_sent.text = messageViewModel.createdAt
         }
     }
 
     internal class MessageReceivedViewHolder(itemView: View) : MessageViewHolder(itemView) {
         override fun bind(messageViewModel: MessageViewModel) {
-//            itemView.text_message.text = messageViewModel.message
+            itemView.text_message_received.text = messageViewModel.message
+            itemView.date_time_received.text = messageViewModel.createdAt
         }
     }
 }
