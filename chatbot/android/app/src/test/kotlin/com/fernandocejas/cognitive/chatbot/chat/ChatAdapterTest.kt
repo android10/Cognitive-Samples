ff934042d7f8d794c5803d972e24192b3e8998ef
Fernando Cejas <fcejas@gmail.com>
2018-02-01 13:17:47 +0100

    Add tests for ChatAdapter. Include Android resurces to fix robolectric not beeing compatible with appt2.

---
+++ chatbot/android/app/src/test/kotlin/com/fernandocejas/cognitive/chatbot/chat/ChatAdapterTest.kt
@@ -0,0 +1,46 @@
+package com.fernandocejas.cognitive.chatbot.chat
+
+import com.fernandocejas.cognitive.chatbot.AndroidTest
+import com.fernandocejas.cognitive.chatbot.R
+import com.fernandocejas.cognitive.chatbot.chat.MessageViewModel.Type.RECEIVED
+import com.fernandocejas.cognitive.chatbot.chat.MessageViewModel.Type.SENT
+import com.fernandocejas.cognitive.chatbot.util.DateTime
+import org.amshove.kluent.shouldBe
+import org.amshove.kluent.shouldEqual
+import org.junit.Before
+import org.junit.Test
+
+class ChatAdapterTest : AndroidTest() {
+
+    private lateinit var chatAdapter: ChatAdapter
+
+    @Before fun setUp() {
+        chatAdapter = ChatAdapter(context())
+    }
+
+    @Test fun `should initialize with greeting message`() {
+        chatAdapter.messages.size shouldBe 1
+        chatAdapter.messages[0].message shouldBe context().getString(R.string.welcome_message)
+    }
+
+    @Test fun `item count should return message collection size`() {
+        chatAdapter.messages.add(MessageViewModel("Test 01", DateTime.now().time(), SENT))
+        chatAdapter.messages.add(MessageViewModel("Test 02", DateTime.now().time(), RECEIVED))
+
+        chatAdapter.itemCount shouldBe chatAdapter.messages.size
+    }
+
+    @Test fun `should return correct item view type`() {
+        chatAdapter.messages.add(MessageViewModel("Test 01", DateTime.now().time(), SENT))
+        chatAdapter.messages.add(MessageViewModel("Test 02", DateTime.now().time(), RECEIVED))
+        chatAdapter.messages.add(MessageViewModel("Test 02", DateTime.now().time(), RECEIVED))
+
+        chatAdapter.getItemViewType(0) shouldEqual R.layout.item_message_received
+        chatAdapter.getItemViewType(1) shouldEqual R.layout.item_message_sent
+        chatAdapter.getItemViewType(2) shouldEqual R.layout.item_message_received
+        chatAdapter.getItemViewType(3) shouldEqual R.layout.item_message_received
+
+        chatAdapter.messages.size shouldBe 4
+        chatAdapter.itemCount shouldBe 4
+    }
+}
