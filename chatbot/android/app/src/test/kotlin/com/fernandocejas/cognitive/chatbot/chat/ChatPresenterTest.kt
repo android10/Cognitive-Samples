64e01eab68e096148cf4a80c98ffe8a1506edaf9
Fernando Cejas <fcejas@gmail.com>
2018-02-06 14:49:22 +0100

    Add more tests for ChatPresenter class.

--- chatbot/android/app/src/test/kotlin/com/fernandocejas/cognitive/chatbot/chat/ChatPresenterTest.kt
+++ chatbot/android/app/src/test/kotlin/com/fernandocejas/cognitive/chatbot/chat/ChatPresenterTest.kt
@@ -2,6 +2,7 @@ package com.fernandocejas.cognitive.chatbot.chat
 
 import com.fernandocejas.cognitive.chatbot.UnitTest
 import com.fernandocejas.cognitive.chatbot.chat.MessageViewModel.Type.SENT
+import com.fernandocejas.cognitive.chatbot.framework.UseCase.None
 import com.fernandocejas.cognitive.chatbot.util.DateTime
 import com.nhaarman.mockito_kotlin.any
 import com.nhaarman.mockito_kotlin.eq
@@ -23,6 +24,12 @@ class ChatPresenterTest : UnitTest() {
         chatPresenter.chatView = chatView
     }
 
+    @Test fun `should execute StartConversation use case and render response message`() {
+        chatPresenter.startConversation()
+
+        verify(startConversation).execute(any(), any())
+    }
+
     @Test fun `should render sent message`() {
         val message = MessageViewModel("Test", DateTime.now(), SENT)
         chatPresenter.sendMessage(message)
@@ -30,7 +37,7 @@ class ChatPresenterTest : UnitTest() {
         verify(chatView).renderMessage(message)
     }
 
-    @Test fun `should execute use case and render response message`() {
+    @Test fun `should execute SendMessage use case and render response message`() {
         val message = MessageViewModel("Test", DateTime.now(), SENT)
         chatPresenter.sendMessage(message)
 
