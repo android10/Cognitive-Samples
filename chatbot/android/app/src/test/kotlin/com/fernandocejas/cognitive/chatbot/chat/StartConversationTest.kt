1a5f7a2c30410ffd3afe4255b47db26ac9ecd06c
Fernando Cejas <fcejas@gmail.com>
2018-02-06 14:54:45 +0100

    Test for StartConversation use case.

--- chatbot/android/app/src/test/kotlin/com/fernandocejas/cognitive/chatbot/chat/StartConversationTest.kt
+++ chatbot/android/app/src/test/kotlin/com/fernandocejas/cognitive/chatbot/chat/StartConversationTest.kt
@@ -1,6 +1,7 @@
 package com.fernandocejas.cognitive.chatbot.chat
 
 import com.fernandocejas.cognitive.chatbot.AndroidTest
+import com.fernandocejas.cognitive.chatbot.framework.UseCase.None
 import com.fernandocejas.cognitive.chatbot.util.DateTime
 import com.nhaarman.mockito_kotlin.verify
 import kotlinx.coroutines.experimental.runBlocking
@@ -8,22 +9,21 @@ import org.junit.Before
 import org.junit.Test
 import org.mockito.Mock
 
-class SendMessageTest : AndroidTest() {
+class StartConversationTest : AndroidTest() {
 
-    private lateinit var sendMessage: SendMessage
+    private lateinit var startConversation: StartConversation
 
     @Mock private lateinit var chatDataSource: ChatDataSource
 
-    @Before fun setUp() {
-        sendMessage = SendMessage(chatDataSource)
+    @Before
+    fun setUp() {
+        startConversation = StartConversation(chatDataSource)
     }
 
+    @Test
     fun `should delegate message response to service`() {
-        val message = Message("test", DateTime.now())
-        val params = SendMessage.Params(message)
+        runBlocking { startConversation.execute({}, None()) }
 
-        runBlocking { sendMessage.execute({}, params) }
-
-        verify(chatDataSource).sendMessage(params.requestMessage)
+        verify(chatDataSource).startConversation()
     }
 }
