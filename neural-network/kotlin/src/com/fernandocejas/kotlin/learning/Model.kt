8a0c56eef6b59ece6627b5093b18f1febba0c44f
Fernando Cejas <fcejas@gmail.com>
2018-08-06 19:48:59 +0200

    Add method for rounding a Double value.

--- neural-network/kotlin/src/com/fernandocejas/kotlin/learning/Model.kt
+++ neural-network/kotlin/src/com/fernandocejas/kotlin/learning/Model.kt
@@ -1,14 +1,19 @@
 package com.fernandocejas.kotlin.learning
 
 class Model {
-    private val inputs = arrayOf(0, 1, 0, 0)
-    private val weights = arrayOf(0, 0, 0, 0)
+    private val inputs = arrayOf(0.0, 1.0, 0.0, 0.0)
+    private val weights = arrayOf(0.0, 0.0, 0.0, 0.0)
 
     fun execute() = evaluateNetwork(inputs, weights)
 
-    private fun evaluateNetwork(inputs: Array<Int>, weights: Array<Int>) {
-        var result = 0
+    private fun evaluateNetwork(inputs: Array<Double>, weights: Array<Double>) {
+        var result = 0.0
         inputs.forEachIndexed { index, input -> result += (input * weights[index]) }
-        println("Result: $result")
+        println("Result: ${roundToDecimals(result, 2)}")
+    }
+
+    private fun roundToDecimals(number: Double, numDecimalPlaces: Int): Double {
+        val factor = Math.pow(10.0, numDecimalPlaces.toDouble())
+        return Math.round(number * factor) / factor
     }
 }
